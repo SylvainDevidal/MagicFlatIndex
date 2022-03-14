1d465cb54b0bcaae9007f7222b037bb899603ada
Sylvain Devidal <administrateur@manga-torii.com>
2022-03-14 09:21:52 +0100

    Quelques modifications du test

--- TestFlatFile/Program.cs
+++ TestFlatFile/Program.cs
@@ -108,28 +108,18 @@ namespace TestFlatFile
                 Console.WriteLine($"Average time per record : {sw.ElapsedTicks / found} ticks");
 
                 Console.WriteLine($"The file contains {personFile.CountRecords()} names");
-                /*
-                persons = personFile.SelectAll();
-                Console.WriteLine("Persons in the file :");
-                foreach (Person p in persons)
-                {
-                    Console.WriteLine($"   {p}");
-                }
+
+                Console.WriteLine("Shrink the file (just fill the holes)...");
+                sw = Stopwatch.StartNew();
                 personFile.Shrink(false);
-                persons = personFile.SelectAll();
-                Console.WriteLine("Persons in the file after shrink :");
-                foreach (Person p in persons)
-                {
-                    Console.WriteLine($"   {p}");
-                }
+                sw.Stop();
+                Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+
+                Console.WriteLine("Reorder the whole file...");
+                sw = Stopwatch.StartNew();
                 personFile.Shrink(true);
-                persons = personFile.SelectAll();
-                Console.WriteLine("Persons in the file after reorder :");
-                foreach (Person p in persons)
-                {
-                    Console.WriteLine($"   {p}");
-                }
-                */
+                sw.Stop();
+                Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
             }
         }
     }
